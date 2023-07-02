24192ef2afe11724a5dfc05d4c5fbc13b582d856
Hassan Toheeb <hassantoheebayinde@gmail.com>
2023-07-02 17:20:15 +0100

    working with es6 classes

---
+++ 0x02-ES6_classes/main.js
@@ -0,0 +1,91 @@
+/* import ClassRoom from "./0-classroom.js";
+const room = new ClassRoom(10);
+console.log(room._maxStudentsSize); */
+
+/* import initializeRooms from './1-make_classrooms.js';
+console.log(initializeRooms());
+bob@dylan:~$ 
+bob@dylan:~$ npm run dev 1-main.js 
+[
+  ClassRoom { _maxStudentsSize: 19 },
+  ClassRoom { _maxStudentsSize: 20 },
+  ClassRoom { _maxStudentsSize: 34 }
+] */
+
+/* import HolbertonCourse from "./2-hbtn_course.js";
+const c1 = new HolbertonCourse("ES6", 1, ["Bob", "Jane"])
+console.log(c1.name);
+c1.name = "Python 101";
+console.log(c1);
+try {
+    c1.name = 12;
+} 
+catch(err) {
+    console.log(err);
+}
+try {
+    const c2 = new HolbertonCourse("ES6", "1", ["Bob", "Jane"]);
+}
+catch(err) {
+    console.log(err);
+} */
+
+/* import Currency from "./3-currency.js";
+const dollar = new Currency('$', 'Dollars');
+console.log(dollar.displayFullCurrency()); */
+
+/* import Pricing from './4-pricing.js';
+import Currency from './3-currency.js';
+const p = new Pricing(100, new Currency("EUR", "Euro"))
+console.log(p);
+console.log(p.displayFullPrice()); */
+
+/* import Building from './5-building.js';
+const b = new Building(100);
+console.log(b);
+class TestBuilding extends Building {}
+try {
+  new TestBuilding(200)
+}
+catch(err) {
+  console.log(err);
+} */
+
+/* `import SkyHighBuilding from './6-sky_high.js';
+const building = new SkyHighBuilding(140, 60);
+console.log(building.sqft);
+console.log(building.floors);
+console.log(building.evacuationWarningMessage());` */
+
+/* import Airport from "./7-airport.js";
+const airportSF = new Airport('San Francisco Airport', 'SFO');
+console.log(airportSF);
+console.log(airportSF.toString()); */
+
+/* import HolbertonClass from "./8-hbtn_class.js";
+const hc = new HolbertonClass(12, "Mezzanine")
+console.log(Number(hc));
+console.log(String(hc)); */
+
+/* import listOfStudents from "./9-hoisting.js";
+console.log(listOfStudents);
+const listPrinted = listOfStudents.map(
+  student => student.fullStudentDescription
+);
+console.log(listPrinted) */
+
+/* import Car from "./10-car.js";
+class TestCar extends Car {}
+const tc1 = new TestCar("Nissan", "Turbo", "Pink");
+const tc2 = tc1.cloneCar();
+console.log(tc1);
+console.log(tc1 instanceof TestCar);
+console.log(tc2);
+console.log(tc2 instanceof TestCar);
+console.log(tc1 == tc2); */
+
+/* import EVCar from './100-evcar.js';
+const ec1 = new EVCar("Tesla", "Turbo", "Red", "250");
+console.log(ec1);
+const ec2 = ec1.cloneCar();
+console.log(ec2); */
